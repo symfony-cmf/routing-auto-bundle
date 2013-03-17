dfee57b5ad19ede29f65db38e1bfee2e57de3c1d
dantleech <dan.t.leech@gmail.com>
2013-03-17 16:42:36 +0000

    Added AutoRouteMaker logic

--- AutoRoute/AutoRouteMaker.php
+++ AutoRoute/AutoRouteMaker.php
@@ -2,25 +2,83 @@
 
 namespace Symfony\Cmf\Bundle\RoutingAutoBundle\AutoRoute;
 
-use Symfony\Cmf\Bundle\RoutingAutoBundle\AutoRoute\RouteStack\Builder;
-use Symfony\Cmf\Bundle\RoutingAutoBundle\AutoRoute\RouteStack\BuilderUnitInterface;
+use Doctrine\ODM\PHPCR\DocumentManager;
+use Symfony\Cmf\Bundle\RoutingAutoBundle\Document\AutoRoute;
 
 /**
  * @author Daniel Leech <daniel@dantleech.com>
  */
 class AutoRouteMaker
 {
-    protected $builder;
-    protected $builderUnit;
+    protected $dm;
 
-    public function __construct(Builder $builder, BuilderUnitInterface $builderUnit)
+    public function __construct(DocumentManager $dm)
     {
-        $this->builder = $builder;
-        $this->builderUnit = $builderUnit;
+        $this->dm = $dm;
     }
 
     public function createOrUpdateAutoRoute(AutoRouteStack $autoRouteStack)
     {
-        $this->builder->build($autoRouteStack, $this->builderUnit);
+        $content = $autoRouteStack->getContext()->getContent();
+
+        $autoRoute = $this->getAutoRouteForDocument($content);
+
+        if (null === $autoRoute) {
+            $autoRoute = new AutoRoute;
+            $autoRoute->setRouteContent($content);
+        }
+
+        $autoRouteStack->addRoute($autoRoute);
+    }
+
+    protected function getAutoRouteForDocument($document)
+    {
+        $dm = $this->dm;
+        $uow = $dm->getUnitOfWork();
+
+        $referrers = $this->dm->getReferrers($document);
+
+        if ($referrers->count() == 0) {
+            return null;
+        }
+
+        // Filter non auto-routes
+        $referrers = $referrers->filter(function ($referrer) {
+            if ($referrer instanceof AutoRoute) {
+                return true;
+            }
+
+            return false;
+        });
+
+        $metadata = $dm->getClassMetadata(get_class($document));
+
+        $locale = null; // $uow->getLocale($document, $locale);
+
+        // If the document is translated, filter locales
+        if (null !== $locale) {
+            throw new \Exception(
+                'Translations not yet supported for Auto Routes - '.
+                'Should be easy.'
+            );
+
+            // array_filter($referrers, function ($referrer) use ($dm, $uow, $locale) {
+            //     $metadata = $dm->getClassMetadata($refferer);
+            //     if ($locale == $uow->getLocaleFor($referrer, $referrer)) {
+            //         return true;
+            //     }
+
+            //     return false;
+            // });
+        }
+
+        if ($referrers->count() > 1) {
+            throw new \RuntimeException(sprintf(
+                'More than one auto route for document "%s"',
+                get_class($document)
+            ));
+        }
+
+        return $referrers->first();
     }
 }
--- Tests/AutoRoute/AutoRouteMakerTest.php
+++ Tests/AutoRoute/AutoRouteMakerTest.php
@@ -3,39 +3,94 @@
 namespace Symfony\Cmf\Bundle\RoutingAutoBundle\Tests\AutoRoute;
 
 use Symfony\Cmf\Bundle\RoutingAutoBundle\AutoRoute\AutoRouteMaker;
+use Doctrine\Common\Collections\ArrayCollection;
 
 class AutoRouteMakerTest extends \PHPUnit_Framework_TestCase
 {
     public function setUp()
     {
-        $this->builder = $this->getMockBuilder(
-            'Symfony\Cmf\Bundle\RoutingAutoBundle\AutoRoute\RouteStack\Builder'
+        $this->dm = $this->getMockBuilder(
+            'Doctrine\ODM\PHPCR\DocumentManager'
         )->disableOriginalConstructor()->getMock();
 
-        $this->builderUnit = $this->getMock(
-            'Symfony\Cmf\Bundle\RoutingAutoBundle\AutoRoute\RouteStack\BuilderUnitInterface'
-        );
+        $this->uow = $this->getMockBuilder(
+            'Doctrine\ODM\PHPCR\UnitOfWork'
+        )->disableOriginalConstructor()->getMock();
 
-        $this->autoRouteStack = $this->getMockBuilder(
+        $this->metadata = $this->getMockBuilder(
+            'Doctrine\ODM\PHPCR\Mapping\ClassMetadata'
+        )->disableOriginalConstructor()->getMock();
+
+        $this->arm = new AutoRouteMaker($this->dm);
+        $this->doc = new \stdClass;
+
+        $this->autoRoute1 = $this->getMock(
+            'Symfony\Cmf\Bundle\RoutingAutoBundle\Document\AutoRoute'
+        );
+        $this->autoRoute2 = $this->getMock(
+            'Symfony\Cmf\Bundle\RoutingAutoBundle\Document\AutoRoute'
+        );
+        $this->nonAutoRoute = new \stdClass;
+
+        $this->autoRouteStack =  $this->getMockBuilder(
             'Symfony\Cmf\Bundle\RoutingAutoBundle\AutoRoute\AutoRouteStack'
         )->disableOriginalConstructor()->getMock();
 
-        $this->autoRouteMaker = new AutoRouteMaker($this->builder, $this->builderUnit);
+        $this->builderContext = $this->getMock(
+            'Symfony\Cmf\Bundle\RoutingAutoBundle\AutoRoute\BuilderContext'
+        );
     }
 
-    public function testCreateOrUpdateAutoRoute()
+    public function testCreateOrUpdateAutoRouteForExisting()
+    {
+        $this->dm->expects($this->once())
+            ->method('getReferrers')
+            ->will($this->returnValue(new ArrayCollection(array(
+                $this->autoRoute1,
+                $this->nonAutoRoute
+            ))));
+
+        $this->autoRouteStack->expects($this->once())
+            ->method('getContext')
+            ->will($this->returnValue($this->builderContext));
+
+        $this->builderContext->expects($this->once())
+            ->method('getContent')
+            ->will($this->returnValue($this->doc));
+
+        $this->dm->expects($this->once())
+            ->method('getUnitOfWork')
+            ->will($this->returnValue($this->uow));
+
+        $this->autoRouteStack->expects($this->once())
+            ->method('addRoute')
+            ->with($this->autoRoute1);
+
+        $this->arm->createOrUpdateAutoRoute($this->autoRouteStack);
+    }
+
+    public function testCreateOrUpdateAutoRouteForNew()
     {
         $testCase = $this;
+        $this->dm->expects($this->once())
+            ->method('getReferrers')
+            ->will($this->returnValue(new ArrayCollection(array(
+            ))));
 
-        $this->builder->expects($this->once())
-            ->method('build')
-            ->will($this->returnCallback(function ($stack, $builderUnit) use ($testCase) {
-                $testCase->assertInstanceOf(
-                    'Symfony\Cmf\Bundle\RoutingAutoBundle\AutoRoute\RouteStack', 
-                    $stack
-                );
+        $this->autoRouteStack->expects($this->once())
+            ->method('getContext')
+            ->will($this->returnValue($this->builderContext));
+
+        $this->builderContext->expects($this->once())
+            ->method('getContent')
+            ->will($this->returnValue($this->doc));
+
+        $this->autoRouteStack->expects($this->once())
+            ->method('addRoute')
+            ->will($this->returnCallback(function ($route) use ($testCase) {
+                $testCase->assertInstanceOf('Symfony\Cmf\Bundle\RoutingAutoBundle\Document\AutoRoute', $route);
             }));
 
-        $stack = $this->autoRouteMaker->createOrUpdateAutoRoute($this->autoRouteStack);
+        $this->arm->createOrUpdateAutoRoute($this->autoRouteStack);
     }
 }
