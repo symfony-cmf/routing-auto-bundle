41fbd65fe0a007be9b7e66e8994f1fc1513c5348
Wouter J <waldio.webdesign@gmail.com>
2013-11-05 16:39:32 +0100

    Fixed bundle

--- DependencyInjection/CmfRoutingAutoExtension.php
+++ DependencyInjection/CmfRoutingAutoExtension.php
@@ -30,7 +30,7 @@ class CmfRoutingAutoExtension extends Extension
         $chainFactoryDef = $container->getDefinition('cmf_routing_auto.factory');
 
         // normalize configuration
-        foreach ($config['auto_route_mapping'] as $classFqn => $config) {
+        foreach ($config['auto_route_mappings'] as $classFqn => $config) {
             $chainFactoryDef->addMethodCall('registerMapping', array($classFqn, $config));
         }
     }
