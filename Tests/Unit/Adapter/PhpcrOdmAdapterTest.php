24b5d048257665f86e992e11edfdb313f2456871
WouterJ <waldio.webdesign@gmail.com>
2015-10-28 10:53:11 +0100

    Rely on built-in prophecy support

--- Tests/Unit/Adapter/PhpcrOdmAdapterTest.php
+++ Tests/Unit/Adapter/PhpcrOdmAdapterTest.php
@@ -12,10 +12,9 @@
 
 namespace Symfony\Cmf\Component\RoutingAuto\Tests\Unit\Adapter;
 
-use Symfony\Cmf\Component\RoutingAuto\Tests\Unit\BaseTestCase;
 use Symfony\Cmf\Bundle\RoutingAutoBundle\Adapter\PhpcrOdmAdapter;
 
-class PhpcrOdmAdapterTest extends BaseTestCase
+class PhpcrOdmAdapterTest extends \PHPUnit_Framework_TestCase
 {
     protected $dm;
     protected $baseRoutePath;
